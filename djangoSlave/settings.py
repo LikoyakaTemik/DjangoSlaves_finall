eee1ca96c23b648febfc6644296733ba4d9c84b4
LikoyakaTemik <92818915+LikoyakaTemik@users.noreply.github.com>
2022-05-27 21:10:25 +0300

    Update settings.py

--- djangoSlave/settings.py
+++ djangoSlave/settings.py
@@ -136,6 +136,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
-"""LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/profile/"
-LOGOUT_REDIRECT_URL = "/""""
